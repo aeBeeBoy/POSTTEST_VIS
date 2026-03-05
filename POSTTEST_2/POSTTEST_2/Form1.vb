f64331a7a9fd5d93c67a1589f2376ada85b191eb
aeBeeBoy <habyby1907@gmail.com>
2026-03-05 16:52:56 +0800

    Add files via upload

---
+++ POSTTEST_2/POSTTEST_2/Form1.vb
@@ -0,0 +1,56 @@
+﻿Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        DataGridView1.ColumnCount = 2
+        DataGridView1.Columns(0).Name = "Judul Buku"
+        DataGridView1.Columns(1).Name = "Genre"
+
+        DataGridView1.AllowUserToAddRows = False
+        DataGridView1.ReadOnly = True
+
+    End Sub
+
+    Private Sub TampilkanData()
+
+        DataGridView1.Rows.Clear()
+
+        For i As Integer = 0 To jumlahBuku - 1
+            DataGridView1.Rows.Add(daftarBuku(i, 0), daftarBuku(i, 1))
+        Next
+
+    End Sub
+
+    Private Sub btnTambah_Click(sender As Object, e As EventArgs) Handles btnTambah.Click
+
+        If txtJudul.Text = "" Or txtGenre.Text = "" Then
+            MessageBox.Show("Judul dan Genre harus diisi!")
+            Exit Sub
+        End If
+
+        TambahBuku(txtJudul.Text, txtGenre.Text)
+
+        TampilkanData()
+
+        txtJudul.Clear()
+        txtGenre.Clear()
+
+    End Sub
+
+    Private Sub btnHapus_Click(sender As Object, e As EventArgs) Handles btnHapus.Click
+
+        Dim index As Integer = CariIndexBuku(txtHapus.Text)
+
+        If index = -1 Then
+            MessageBox.Show("Buku tidak ditemukan!")
+        Else
+            HapusBuku(index)
+            TampilkanData()
+            MessageBox.Show("Buku berhasil dihapus!")
+        End If
+
+        txtHapus.Clear()
+
+    End Sub
+
+End Class
---
+++ POSTTEST_2/POSTTEST_2/Module1.vb
@@ -0,0 +1,31 @@
+﻿Public Module ModuleBuku
+
+    Public daftarBuku(99, 1) As String
+    Public jumlahBuku As Integer = 0
+
+    Public Sub TambahBuku(ByVal judul As String, ByVal genre As String)
+        If jumlahBuku < 100 Then
+            daftarBuku(jumlahBuku, 0) = judul
+            daftarBuku(jumlahBuku, 1) = genre
+            jumlahBuku += 1
+        End If
+    End Sub
+
+    Public Function CariIndexBuku(ByVal judul As String) As Integer
+        For i As Integer = 0 To jumlahBuku - 1
+            If daftarBuku(i, 0).ToLower() = judul.ToLower() Then
+                Return i
+            End If
+        Next
+        Return -1
+    End Function
+
+    Public Sub HapusBuku(ByVal index As Integer)
+        For i As Integer = index To jumlahBuku - 2
+            daftarBuku(i, 0) = daftarBuku(i + 1, 0)
+            daftarBuku(i, 1) = daftarBuku(i + 1, 1)
+        Next
+        jumlahBuku -= 1
+    End Sub
+
+End Module
